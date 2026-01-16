d30b36002cb744bc8c3527f599a0d5d0cdf93a92
VioletsOleander <1377232072@qq.com>
2026-01-16 14:27:25 +0800

    feat: add support to wiki section
    Also optimize the output messages.

--- src/vanillian/main.py
+++ src/vanillian/main.py
@@ -1,5 +1,7 @@
 import argparse
 import logging
+import sys
+from pathlib import Path
 
 from .log.canonicalizer import LogCanonicalizer
 from .log.constants import LOG_PATH
@@ -13,24 +15,43 @@ logger = logging.getLogger(__name__)
 
 
 def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description="The dedicated steward for my vanilla vault.")
+    prog = Path(sys.argv[0]).name
+    parser = argparse.ArgumentParser(
+        prog=prog,
+        description="The dedicated steward for vault-vanilla.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
     subparsers = parser.add_subparsers(dest="command", required=True)
-
-    parser_log = subparsers.add_parser("log", help="Canonicalize log items")
-    parser_log.add_argument(
+    log_parser = subparsers.add_parser(
+        "log",
+        description="The canonicalizer for the vault log file.",
+        help="Canonicalize log lines in the vault log file",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    log_parser.add_argument(
+        "file",
+        nargs="?",
+        default="logs/Personal Log.md",
+        help="Path to the log file to be canonicalized",
+    )
+    log_parser.add_argument(
         "-f",
         "--force",
         action="store_true",
-        help="Not ask for confirmation before canonicalizing each log item",
+        help="Do not ask for confirm before canonicalizing each log line",
     )
-    parser_log.add_argument(
+    log_parser.add_argument(
         "--overwrite",
         action="store_true",
-        help="Overwrite the original log file, otherwise write to another .canonicalized suffixed file",
+        help="Overwrite the original log file, otherwise, write to another .canonicalized suffixed file",
     )
 
-    parser_commit = subparsers.add_parser("commit", help="Commit changes to the vault")
-    parser_commit.add_argument(
+    commit_parser = subparsers.add_parser(
+        "commit",
+        help="Commit changes to the vault",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    commit_parser.add_argument(
         "--message", "-m", type=str, required=True, help="The commit message for the changes"
     )
 
@@ -43,7 +64,9 @@ def main() -> int:
     if args.command == "log":
         try:
             lines = read_lines(LOG_PATH)
-            LogCanonicalizer.canonicalize(lines, force=args.force)
+
+            canonicalizer = LogCanonicalizer()
+            canonicalizer.canonicalize(lines, force=args.force)
 
             dest_path = LOG_PATH if args.overwrite else LOG_PATH.with_suffix(".canonicalized.md")
             write_lines_atomic(dest_path, lines)
